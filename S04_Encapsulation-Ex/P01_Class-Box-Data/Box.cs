7987342ce48d97970bb312b47c546ebe2dbcc4c0
Yavor Gornalov <y.gornalov@gmail.com>
2024-04-23 17:56:10 +0300

    Class Box Data --uploaded

---
+++ S04_Encapsulation-Ex/P01_Class-Box-Data/Box.cs
@@ -0,0 +1,73 @@
+﻿
+using System.Text;
+
+namespace Box
+{
+    public class Box
+    {
+        private double length;
+        private double width;
+        private double height;
+
+        public Box(double length, double width, double height)
+        {
+            Length = length;
+            Width = width;
+            Height = height;
+        }
+
+        public double Length
+        {
+            get => length;
+            private set
+            {
+                if (value <= 0)
+                    ThrowValidatioExeption(nameof(this.Length));
+                length = value;
+            }
+        }
+
+        public double Width
+        {
+            get => width;
+            private set
+            {
+                if (value <= 0)
+                    ThrowValidatioExeption(nameof(this.Width));
+                width = value;
+            }
+        }
+
+        public double Height
+        {
+            get => height;
+            private set
+            {
+                if (value <= 0)
+                    ThrowValidatioExeption(nameof(this.Height));
+                height = value;
+            }
+        }
+
+        public double SurfaceArea() => 2 * (Length * Width + Length * Height + Height * Width);
+
+        public double LateralSurfaceArea() => 2 * (Length * Height + Width * height);
+
+        public double Volume() => Length * Width * Height;
+
+        public override string ToString()
+        {
+            var sb = new StringBuilder();
+            sb.AppendLine($"Surface Area - {SurfaceArea():F2}");
+            sb.AppendLine($"Lateral Surface Area - {LateralSurfaceArea():F2}");
+            sb.AppendLine($"Volume - {Volume():F2}");
+
+            return sb.ToString().Trim();
+        }
+
+        private static void ThrowValidatioExeption(string propertyName)
+        {
+            throw new ArgumentException($"{propertyName} cannot be zero or negative.");
+        }
+    }
+}
---
+++ S04_Encapsulation-Ex/P01_Class-Box-Data/StartUp.cs
@@ -0,0 +1,22 @@
+﻿namespace Box
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                var box = new Box(
+                    double.Parse(Console.ReadLine()),
+                    double.Parse(Console.ReadLine()),
+                    double.Parse(Console.ReadLine())
+                );
+                Console.WriteLine(box);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
