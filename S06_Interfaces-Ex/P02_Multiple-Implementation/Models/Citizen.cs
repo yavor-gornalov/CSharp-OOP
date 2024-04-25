d75c930eded8b43fd8688db72e70faedc1ea6dba
Yavor Gornalov <y.gornalov@gmail.com>
2024-04-25 15:39:32 +0300

    Multiple-Implementation --uploaded

---
+++ S06_Interfaces-Ex/P02_Multiple-Implementation/Models/Citizen.cs
@@ -0,0 +1,39 @@
+﻿namespace PersonInfo
+{
+    public class Citizen : IPerson, IIdentifiable, IBirthable
+    {
+        private string _name;
+        private int _age;
+        private string _id;
+        private string _birthdate;
+
+        public Citizen(string name, int age, string id, string birthdate)
+        {
+            Name = name;
+            Age = age;
+            Id = id;
+            Birthdate = birthdate;
+        }
+
+        public string Name
+        {
+            get => _name;
+            set => _name = value;
+        }
+        public int Age
+        {
+            get => _age;
+            set => _age = value;
+        }
+        public string Id
+        {
+            get => _id;
+            set => _id = value;
+        }
+        public string Birthdate
+        {
+            get => _birthdate;
+            set => _birthdate = value;
+        }
+    }
+}
