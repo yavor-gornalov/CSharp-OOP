eb48ef98fdc0727e1199d57ffdf2ed2106397a84
Yavor Gornalov <y.gornalov@gmail.com>
2024-04-25 15:38:54 +0300

    Define-an-Interface-IPerson --uploaded

---
+++ S06_Interfaces-Ex/P01_Define-an-Interface-IPerson/Models/Citizen.cs
@@ -0,0 +1,24 @@
+﻿namespace PersonInfo
+{
+    public class Citizen : IPerson
+    {
+        private string _name;
+        private int _age;
+
+        public Citizen(string name, int age)
+        {
+            Name = name;
+            Age = age;
+        }
+        public string Name
+        {
+            get => _name;
+            set => _name = value;
+        }
+        public int Age
+        {
+            get => _age;
+            set => _age = value;
+        }
+    }
+}
