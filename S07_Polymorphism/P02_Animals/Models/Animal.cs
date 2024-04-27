419018d5da238f31b442345392ab6ff7dfb645fc
Yavor Gornalov <y.gornalov@gmail.com>
2024-04-27 16:00:11 +0300

    Animals --uploaded

---
+++ S07_Polymorphism/P02_Animals/Models/Animal.cs
@@ -0,0 +1,16 @@
+﻿namespace Animals.Models
+{
+    public class Animal
+    {
+        public string Name { get; private set; }
+        public string FavouriteFood { get; private set; }
+
+        public Animal(string name, string favouriteFood)
+        {
+            Name = name;
+            FavouriteFood = favouriteFood;
+        }
+
+        public virtual string ExplainSelf() => $"I am {Name} and my fovourite food is {FavouriteFood}";
+    }
+}
---
+++ S07_Polymorphism/P02_Animals/Models/Cat.cs
@@ -0,0 +1,11 @@
+﻿namespace Animals.Models
+{
+    public class Cat : Animal
+    {
+        public Cat(string name, string favouriteFood) : base(name, favouriteFood)
+        {
+        }
+
+        public override string ExplainSelf() => base.ExplainSelf() + Environment.NewLine + "MEEOW";
+    }
+}
