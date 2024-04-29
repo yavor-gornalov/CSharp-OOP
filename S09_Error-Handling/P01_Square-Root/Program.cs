4a59239caac1058a8fe2e0a3a152ddefbb0554d8
Yavor Gornalov <y.gornalov@gmail.com>
2024-04-29 22:50:16 +0300

    Square Root Problem --uploaded

---
+++ S09_Error-Handling/P01_Square-Root/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace SquareRoot
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                double number = double.Parse(Console.ReadLine());
+                if (number < 0)
+                    throw new ArgumentException("Invalid number.");
+                Console.WriteLine(Math.Sqrt(number));
+            }
+            catch (ArgumentException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                Console.WriteLine("Goodbye.");
+            }
+        }
+    }
+}
