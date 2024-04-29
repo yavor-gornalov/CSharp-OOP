4a8fdfffaf22217fa3a3400b66b2f16596f44036
Yavor Gornalov <y.gornalov@gmail.com>
2024-04-29 22:51:55 +0300

    Sum of Integers Problem --uploaded

---
+++ S09_Error-Handling/P04_Sum-of-Integers/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace SumOfIntegers
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            var arr = Console.ReadLine().Split().ToArray();
+
+            double sum = 0;
+            foreach (var element in arr)
+            {
+                try
+                {
+                    int number = int.Parse(element);
+                    sum += number;
+                }
+                catch (FormatException)
+                {
+                    Console.WriteLine($"The element '{element}' is in wrong format!");
+                }
+                catch (OverflowException)
+                {
+                    Console.WriteLine($"The element '{element}' is out of range!");
+                }
+                finally
+                {
+                    Console.WriteLine($"Element '{element}' processed - current sum: {sum}");
+                }
+            }
+            Console.WriteLine($"The total sum of all integers is: {sum}");
+        }
+    }
+}
