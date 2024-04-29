259efab5f90c25a77ec167586f5e75ee17bb2fc3
Yavor Gornalov <y.gornalov@gmail.com>
2024-04-29 22:50:44 +0300

    Enter Numbers Problem --uploaded

---
+++ S09_Error-Handling/P02_Enter-Numbers/Program.cs
@@ -0,0 +1,38 @@
+﻿
+namespace EnterNumbers
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = ReadNumbers(start: 1, end: 100, count: 10);
+            Console.WriteLine(string.Join(", ", numbers));
+        }
+
+        private static int[] ReadNumbers(int start, int end, int count)
+        {
+            var numbers = new List<int>(count);
+
+            while (numbers.Count < count)
+            {
+                try
+                {
+                    int number = int.Parse(Console.ReadLine());
+                    if (number <= start || number >= end)
+                        throw new ArgumentOutOfRangeException();
+                    numbers.Add(number);
+                    start = number;
+                }
+                catch (FormatException)
+                {
+                    Console.WriteLine("Invalid Number!");
+                }
+                catch (ArgumentOutOfRangeException)
+                {
+                    Console.WriteLine($"Your number is not in range {start} - {end}!");
+                }
+            }
+            return numbers.ToArray();
+        }
+    }
+}
