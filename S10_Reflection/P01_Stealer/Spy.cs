dc979844fa0e8f169c56bc3d3d6c7a34691aed3c
Yavor Gornalov <y.gornalov@gmail.com>
2024-04-30 20:56:39 +0300

    Stealer-Problem --uploaded

---
+++ S10_Reflection/P01_Stealer/Spy.cs
@@ -0,0 +1,28 @@
+﻿using System.Reflection;
+using System.Text;
+
+namespace Stealer
+{
+    public class Spy
+    {
+        public string StealFieldInfo(string investigatedClass, params string[] requestedFields)
+        {
+            Type classType = Type.GetType(investigatedClass);
+            FieldInfo[] classFields = classType.GetFields(
+                BindingFlags.Instance |
+                BindingFlags.Static |
+                BindingFlags.NonPublic |
+                BindingFlags.Public);
+
+
+            object? classInstance = Activator.CreateInstance(classType, Array.Empty<object>());
+
+            var sb = new StringBuilder();
+            sb.AppendLine($"Class under investigation: {investigatedClass}");
+            foreach (var field in classFields.Where(f => requestedFields.Contains(f.Name)))
+                sb.AppendLine($"{field.Name} = {field.GetValue(classInstance)}");
+
+            return sb.ToString().Trim();
+        }
+    }
+}
