3ab88bb2841a44c122e2239d4a82d6538faa7efe
Yavor Gornalov <y.gornalov@gmail.com>
2024-04-30 20:58:17 +0300

    Collector-Problem --uploaded

---
+++ S10_Reflection/P04_Collector/StartUp.cs
@@ -0,0 +1,15 @@
+﻿namespace Stealer
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            Spy spy = new Spy();
+            //string result = spy.StealFieldInfo("Stealer.Hacker", "username", "password");
+            //string result = spy.AnalyzeAccessModifiers("Stealer.Hacker");
+            //string result = spy.RevealPrivateMethods("Stealer.Hacker");
+            string result = spy.CollectGettersAndSetters("Stealer.Hacker");
+            Console.WriteLine(result);
+        }
+    }
+}
