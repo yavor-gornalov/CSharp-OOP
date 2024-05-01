be8db1ca72143b67903350240d0a67f8a83e7afa
Yavor Gornalov <y.gornalov@gmail.com>
2024-05-01 15:41:34 +0300

    Validation-Attributes --uploaded

---
+++ S11_Reflection-Ex/P02_Validation-Attributes/Models/Validator.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using ValidationAttributes.Attributes;
+
+namespace ValidationAttributes.Models
+{
+    public static class Validator
+    {
+        public static bool IsValid(object obj)
+        {
+            Type objType = obj.GetType();
+
+            PropertyInfo[] properties = objType.GetProperties();
+
+            foreach (PropertyInfo propertyInfo in properties)
+            {
+                MyValidationAttribute[] attributes = propertyInfo.GetCustomAttributes<MyValidationAttribute>().ToArray();
+
+                foreach (MyValidationAttribute attribute in attributes)
+                {
+                    if (!attribute.IsValid(propertyInfo.GetValue(obj)))
+                    {
+                        return false;
+                    }
+                }
+            }
+
+            return true;
+        }
+    }
+}
