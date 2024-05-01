19a925a6154dabb6f1faa7de9563c77b0b7a68bb
Yavor Gornalov <y.gornalov@gmail.com>
2024-05-01 15:40:12 +0300

    Command-Pattern --uploaded

---
+++ S11_Reflection-Ex/P01_Command-Pattern/Core/Models/CommandInterpreter.cs
@@ -0,0 +1,33 @@
+﻿using CommandPattern.Core.Contracts;
+using System.Linq;
+using System.Reflection;
+using System;
+
+namespace CommandPattern.Core.Models
+{
+    internal class CommandInterpreter : ICommandInterpreter
+    {
+        public string Read(string args)
+        {
+            var commandTokens = args.Split(" ", StringSplitOptions.RemoveEmptyEntries).ToArray();
+
+            //if (commandTokens.Length <= 1)
+            //    throw new ArgumentException("Missing arguments!");
+
+            var commandName = commandTokens[0];
+            var commandArgs = commandTokens[1..];
+
+            Type commandType = Assembly
+                .GetEntryAssembly()
+                .GetTypes()
+                .FirstOrDefault(x => x.Name == commandName + "Command");
+
+            if (commandType == null)
+                throw new InvalidOperationException("Invalid command!");
+
+            ICommand command = Activator.CreateInstance(commandType) as ICommand;
+
+            return command.Execute(commandArgs);
+        }
+    }
+}
---
+++ S11_Reflection-Ex/P01_Command-Pattern/Core/Models/HelloCommand.cs
@@ -0,0 +1,9 @@
+﻿using CommandPattern.Core.Contracts;
+
+namespace CommandPattern.Core.Models
+{
+    internal class HelloCommand : ICommand
+    {
+        public string Execute(string[] args) => $"Hello, {args[0]}";
+    }
+}
